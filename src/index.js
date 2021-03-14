d1dba209b0e0235d8fbaa18822e2eb4297799cee
Elisha Awuni <awunielisha@gmail.com>
2021-03-14 01:00:09 +0000

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import './bootstrap.css';
+// import App from './App';
+import Navigation from './components/navigation/Navigation';
+import Header from './components/header/Header';
+import About from './components/about-section/About-us';
+import Inventory from './components/inventory-section/Inventory';
+import Contact from './components/contact-section/Contact';
+import Footer from './components/footer/Footer';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Navigation />
+    <Header />
+    <About />
+    <div id="hr">
+        <hr class="container" />
+      </div>
+    <Contact/>
+    <Inventory />
+    <Footer/>
   </React.StrictMode>,
   document.getElementById('root')
 );
